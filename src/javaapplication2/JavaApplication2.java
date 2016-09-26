5d6971bb8b2bbf44f1a72f25f5c23370ed82a2a6
Yash Gupta <guptayash955@gmail.com>
2016-09-26 23:30:10 +0530

    DBMS Project
    DBMS

---
+++ src/javaapplication2/JavaApplication2.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication2;
+import java.sql.*;
+
+/**
+ *
+ * @author Yash
+ */
+public class JavaApplication2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Connection conn=null;
+        try {
+            String driverName="oracle.jdbc.driver.OracleDriver";
+            Class.forName(driverName);
+            String serverName="Johnny";
+            String serverPort ="1521";
+            String sid="XE";
+            String url="jdbc:oracle:thin:@" + serverName + ":" +serverPort + ":" + sid;
+            String username="BCE0662";
+            String password="BCE0662";
+            conn=DriverManager.getConnection(url,username,password);
+            System.out.println("Successfully Connected to the database");
+        }
+        catch (ClassNotFoundException e) {
+            System.out.println("Could not find the database driver" +e.getMessage());
+        } catch (SQLException e) {
+            System.out.println("Could not connect to the database" + e.getMessage());
+            
+        }
+        
+    }
+    
+}
