77a3ea0e65a71aea42722039d25a9b6518c17522
Prateek Goel <goelprateek5@gmail.com>
2016-10-31 23:23:33 +0530

    commit

--- src/Sign_up.java
+++ src/Sign_up.java
@@ -80,10 +80,20 @@ public class Sign_up extends javax.swing.JFrame {
         });
 
         jTextField4.setText("Pre-Filled, Non-editable");
+        jTextField4.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jTextField4ActionPerformed(evt);
+            }
+        });
 
         jTextField8.setText("NULL if New User");
 
         jButton1.setText("Submit");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
 
         jButton2.setText("Cancel");
         jButton2.addActionListener(new java.awt.event.ActionListener() {
@@ -225,6 +235,14 @@ Sign_up SignUp=new Sign_up();
 Sign_up.this.dispose();// TODO add your handling code here:
 
     }//GEN-LAST:event_jButton2ActionPerformed
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jButton1ActionPerformed
+
+    private void jTextField4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jTextField4ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jTextField4ActionPerformed
 
     /**
      * @param args the command line arguments
